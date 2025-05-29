39e2c70810991bf9c76197fc2c768fc545139b54
Mikołaj Pęcak <mikolajpecak@gmail.com>
2025-05-29 14:53:29 +0200

    Add timer

--- app/components/counter/counter.styled.ts
+++ app/components/counter/counter.styled.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #000;
+`;
+
+export const Timer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #222;
+  border-radius: 50%;
+  aspect-ratio: 1/1;
+  padding: 2rem;
+`;
--- app/components/counter/counter.tsx
+++ app/components/counter/counter.tsx
@@ -2,10 +2,12 @@
 
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
 import { ParamName } from '@/enums';
 
-import { Container } from './counter.styled';
+import { Container, Timer } from './counter.styled';
 
 export default function Counter() {
   const searchParams = useSearchParams();
@@ -21,7 +23,14 @@ export default function Counter() {
 
   return (
     <Container>
-      <h1>{event}</h1>
+      <Timer>
+        <FlipClockCountdown
+          to={new Date().getTime() + 48 * 3600 * 1000 + 5000}
+          labelStyle={{
+            color: '#fff',
+          }}
+        />
+      </Timer>
     </Container>
   );
 }
